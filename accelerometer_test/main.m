ab8e7ffaac4acad040b468deb62e844e577b7aa1
민경준 <KyungJunMin@mingyeongjun-ui-MacBook-Pro.local>
2017-03-17 17:56:05 +0900

    Initial Commit

---
+++ accelerometer_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  accelerometer_test
+//
+//  Created by 민경준 on 2017. 3. 17..
+//  Copyright © 2017년 민경준. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
